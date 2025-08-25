b1876d90c5fd50e87953088f47d0210ac0deadd3
EJ Calderon <ejcalderon@gmail.com>
2025-08-25 18:34:54 -0500

    clean up app

--- wakfu-sublimations/src/App.tsx
+++ wakfu-sublimations/src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import { Button, Content, Grid, Column, TextInput, Select, SelectItem, DataTable, Table, TableHead, TableRow, TableHeader, TableBody, TableCell } from '@carbon/react'
+import { Content, Grid, Column } from '@carbon/react'
 import './App.css'
 import HeaderComponent from './components/HeaderComponent'
 import TableComponent from './components/TableComponent'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <HeaderComponent />
-      
       <Content>
         <Grid>
           <Column lg={16} md={8} sm={4}>
